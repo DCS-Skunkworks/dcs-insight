7d561242e17f82b891ea1fcedc2a77d3999ec1c7
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2023-10-15 15:00:43 +0300

    Looping in range testing and option to show only changes

---
+++ src/client/DCSInsight/Misc/Extensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace DCSInsight.Misc
+{
+    using TextBox = System.Windows.Controls.TextBox;
+
+    public static class Extensions
+    {
+        /// <summary>
+        /// Perform a deep Copy of the object, using Json as a serialization method. NOTE: Private members are not cloned using this method.
+        /// </summary>
+        /// <typeparam name="T">The type of object being copied.</typeparam>
+        /// <param name="source">The object instance to copy.</param>
+        /// <returns>The copied object.</returns>
+        public static T CloneJson<T>(this T source)
+        {
+            //Note to devs: Use Newtonsoft.Json to clone the object, not System.Text.Json; because there are a lot of Newtonsoft.Json [JsonIgnore] attributes that are ignored
+            //by the System.Text.Json serializer. This could lead to recursive serialization of unwanted properties that raises an exception.
+
+            if (!typeof(T).IsSerializable)
+            {
+                throw new ArgumentException($"DeepClone error. The type must be serializable");
+            }
+
+            //Following line fixes "Could not create an instance of type xxx Type is an interface or abstract class and cannot be instantiated."
+            //when deep cloning IKeyPressInfo from  AddKeySequence(string description, SortedList<int, IKeyPressInfo> keySequence)
+            var settings = new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.Auto };
+
+            var jsonString = JsonConvert.SerializeObject(source, settings);
+            return JsonConvert.DeserializeObject<T>(jsonString, settings);
+        }
+
+        /// <summary>
+        /// US Culture used. Decimal separator is '.'
+        /// </summary>
+        public static bool ValidateDouble(this TextBox textBox)
+        {
+            if (string.IsNullOrEmpty(textBox.Text))
+            {
+                return true;
+            }
+            
+            return double.TryParse(textBox.Text, NumberStyles.Any, CultureInfo.InvariantCulture, out _);
+        }
+    }
+}
