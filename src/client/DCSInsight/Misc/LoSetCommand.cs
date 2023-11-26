1a773cb597158ffe9e14e49bc5418761ef646cbd
Jerker Dahlblom <jdahlblom@users.noreply.github.com>
2023-11-26 15:59:13 +0200

    LoSetCommand file now external, iCommands searchable
    * All iCommands contained in file iCommands.txt which user can export, info in wiki
    * iCommands can be searched

--- src/client/DCSInsight/Misc/LoSetCommand.cs
+++ src/client/DCSInsight/Misc/LoSetCommand.cs
@@ -1,31 +1,39 @@
-﻿using System;
+﻿using NLog;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows;
+using System.Windows.Shapes;
 
 namespace DCSInsight.Misc
 {
     internal class LoSetCommand
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        private const string CommandsFile = "iCommands.txt";
+
         public string Description { get; private set; }
         public string Code { get; private set; }
 
         internal static List<LoSetCommand> LoadCommands()
         {
             var result = new List<LoSetCommand>();
-            var resourceStream = Application.GetResourceStream(new Uri(@"/dcs-insight;component/Items/iCommands.txt", UriKind.Relative));
-            if (resourceStream == null) return result;
-
-            var streamReader = new StreamReader(resourceStream.Stream);
-            string line;
-
-            while ((line = streamReader.ReadLine()) != null)
+            var loSetCommandsFile = $"{AppDomain.CurrentDomain.BaseDirectory}{CommandsFile}";
+            if (!File.Exists(loSetCommandsFile))
             {
-                if(!line.Trim().StartsWith("i") || !line.Contains('\t') || line.Contains('/')) continue;
+                Logger.Error($"Failed to find {CommandsFile} in base directory.");
+                return result;
+            }
 
-                var array = line.Split('\t');
-                result.Add(new LoSetCommand{Code = array[1].Trim(), Description = array[0].Trim() });
+            var stringArray = File.ReadAllLines(loSetCommandsFile);
+
+            foreach (var s in stringArray)
+            {
+                if (string.IsNullOrEmpty(s) || !s.Trim().StartsWith("i") || !s.Contains('\t') || s.Contains('/') || s.Contains(':')) continue;
+
+                var array = s.Split('\t');
+                result.Add(new LoSetCommand { Code = array[1].Trim(), Description = array[0].Trim() });
             }
 
             result = result.OrderBy(o => o.Description).ToList();
